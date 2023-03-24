ef52897891d4798e044bbfedda723e5f89239d7e
Artem Bogdanov <a.bogdanov@niitp.in>
2023-03-24 13:34:20 +0300

    admin: creation of products

---
+++ src/components/admin/ProductsComponent.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="products" @click="state.edited=false">
+    <h1 class="products__title">Products</h1>
+    <input type="text" class="products__input" v-if="state.edited" v-model="state.editValue.title" @keydown.enter="edit" v-focus @click.stop>
+    <div class="products__content">
+      <div class="products__item" v-for="product in state.products" @click.stop="()=>{
+        state.edited = true
+        state.editValue=product
+      }">
+        <span>{{ product.title }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {onMounted, reactive} from "vue";
+import axios from "axios";
+import {useMainStore} from "@/stores/counter";
+
+const store = useMainStore()
+
+const vFocus = {
+  mounted: (el) => el.focus()
+}
+
+const state = reactive({
+  products: [],
+  editValue: null,
+  edited: false
+})
+
+const edit = async () =>{
+  store.isFetching = true
+  state.edited = false
+  await axios.put(import.meta.env.VITE_BASE_URL + "/api/products/edit",{
+    id: state.editValue.id,
+    title: state.editValue.title
+  }).then(res => {
+    store.isFetching = false
+  })
+  state.editValue = null
+  await init()
+}
+
+const init = async () =>{
+  store.isFetching = true
+  axios.get(import.meta.env.VITE_BASE_URL + "/api/products").then(res => {
+    state.products = res.data
+    store.isFetching = false
+  })
+}
+
+onMounted(async () => {
+  await init()
+})
+</script>
+
+<style lang="scss" scoped>
+.products {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  &__title{
+    text-align: center;
+  }
+  &__content {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 1000px;
+    justify-content: space-between;
+  }
+  &__item{
+    padding: 10px;
+    margin: 10px;
+    border-radius: 5px;
+    border:  1px solid #505050;
+    cursor: pointer;
+    transition: 0.2s;
+    &:hover{
+      scale: 1.1;
+      transition: 0.2s;
+    }
+  }
+  &__input{
+    border: none;
+    width: auto;
+    box-shadow: 0 0 10px rgba(1,1,1,0.2);
+    padding: 10px;
+    margin: 30px;
+    border-radius: 5px;
+    outline: none;
+  }
+}
+</style>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,15 +1,46 @@
 <template>
-  <div>
-<h1>Adminka</h1>
+  <div class="admin">
+    <div class="admin__content">
+      <h1>Админка</h1>
+      <div class="admin__buttonBlock">
+        <el-button @click="state.selectedBlock = pages.meals">Блюда</el-button>
+        <el-button @click="state.selectedBlock = pages.products">Ингредиенты</el-button>
+      </div>
+      <MealsComponent class="admin__page" v-if="state.selectedBlock===pages.meals"/>
+      <ProductsComponent class="admin__page" v-if="state.selectedBlock===pages.products"/>
+    </div>
   </div>
+  <Fetching v-if="store.isFetching"/>
 </template>
 
-<script>
-export default {
+<script setup>
+import {reactive} from "vue";
+import MealsComponent from "@/components/admin/MealsComponent.vue";
+import ProductsComponent from "@/components/admin/ProductsComponent.vue";
+import Fetching from "@/components/Fetching.vue";
+import {useMainStore} from "@/stores/counter";
 
-}
+const store = useMainStore()
+
+const pages =  {meals: 'meals', products: 'products'}
+
+const state = reactive({
+  selectedBlock: pages.meals
+})
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.admin{
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: Dialog, sans-serif;
+  &__content{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+}
 </style>
